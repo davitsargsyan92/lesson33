94c5359f22af7f843e10f8ce2cc35c382c872172
davit.g.sargsyan <davit.g.sargsyan@digitain.com>
2021-03-17 21:16:26 +0400

    lazy components

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React , {lazy , Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import Header from "./components/common/Header";
-import List from './components/list/list';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import NotFound from './components/notFound/NotFound';
-import Detail from "./components/detail/Detail";
-import Favorites from './components/favorites/index'
 import './index.css';
 
+const List = lazy(() => import("./components/list/list"));
+const Detail = lazy(() => import("./components/detail/Detail"));
+const Favorites = lazy(() => import("./components/favorites/index"));
+const NotFound = lazy(() => import("./components/notFound/NotFound"));
 const App = () => {
 
   return (
       <BrowserRouter>
           <div>
-              <Header/>
-              <Switch>
-                <Route path="/" component={List} exact/> 
-                <Route path="/currency/:currencyId" component={Detail}  exact />
-                <Route path="/favorites" component={Favorites}  exact />
+              <Suspense fallback={ <div>Fallback</div>}>
+                  <Header/>
+                  <Switch>
+                      <Route path="/" component={List} exact/>
+                      <Route path="/currency/:currencyId" component={Detail}  exact />
+                      <Route path="/favorites" component={Favorites}  exact />
+                      <Route component={NotFound} />
+                  </Switch>
+              </Suspense>
 
-                <Route component={NotFound} /> 
-              </Switch>
           </div>
       </BrowserRouter>
 
